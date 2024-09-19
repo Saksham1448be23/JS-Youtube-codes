4c63a5cec7a2fbe6c0fde59bdd97d5a85e5f9210
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2024-09-19 16:19:34 +0000

    05

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -1,2 +1,16 @@
 let n=99;
 console.log(typeof n);
+console.log(typeof null);
+console.log(typeof undefined);
+/*
+TYPES OF DATATYPES
+
+number= 2 to power 53
+bigint
+string=""
+boolean=true false
+null=standalone value
+undefined=
+symbol= unique
+object
+*/
