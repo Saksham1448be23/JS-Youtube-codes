1631ff7e8d7a8e87c2b38abd0b7c7eb71eb25532
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2024-09-19 13:45:58 +0000

    04

---
+++ Basics/variableInTableFormandBasicInitailizingThem.js
@@ -0,0 +1,10 @@
+const Id=1448;
+let accEmail="SS@gmail.com";
+var accCity="Solan";
+console.log([Id,accEmail,accCity]);
+console.table([Id,accCity,accEmail]); // gve output in table form and their entity names in form of index
+k="king"; // It will work but is not recommended
+console.log(k);
+/*
+Don't use var because of issue in block and function scope
+*/
