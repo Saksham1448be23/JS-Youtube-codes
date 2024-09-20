e47b4a0052e929dbb4e0b0994d21aa16b7da97db
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2024-09-20 03:14:49 +0000

    06

--- Basics/ConvertingDataType.js
+++ Basics/ConvertingDataType.js
@@ -1,6 +1,20 @@
 let ss=null; //null is object
-console.log(typeof ss);
-console.log(Number(ss)); //converts to number so 0
+// console.log(typeof ss);
+// console.log(Number(ss)); //converts to number so 0
 let s="99"; 
-console.log(typeof s);
-console.log(Number(s));
+// console.log(typeof s);
+// console.log(Number(s));
+
+//*******OPERATION************//
+
+let a=9;
+let na=-a;
+// console.log(na);  output -9
+
+console.log("1"+2); //12
+console.log(2+"1"); //21
+console.log(2+"1"+2); //212
+console.log(1+2+"2");//32
+console.log((3+4)*5%3); //2
+console.log(true); //true
+console.log(+true); //1
