64ceb577632a69f4c9e39bcd58213dd98475c5d1
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2024-09-22 11:32:18 +0000

    11

---
+++ Basics/String.js
@@ -0,0 +1,20 @@
+const name="kkk";
+const repo=90;
+// console.log(name+repo+"value");//  outdated way of useing syntax use backsylash
+console.group(`Hello my name is ${name} and repo is ${repo}`);
+ const game= new String('Jitega'); // object banane k liye new use kiya h , aur ye stack memory h
+ console.log(game[4]);
+
+ console.log(game.length); // lenght of string game
+
+const newgame= game.substring(0,2); //  (x,y) x=index of string ,y=the length 
+ console.log(newgame);
+
+const newgmae2=game.slice(-8,3); // x index value , y length
+console.log(newgmae2);
+
+const nw="   ggg   ";
+console.log(nw);
+console.log(nw.trim()); // starting and ending space ko kaat deta h trim()-- trimStart(),trimEnd();
+
+
