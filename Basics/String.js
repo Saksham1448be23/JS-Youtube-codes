0db2d4795ef582dfcb724bf92d2855f4e9d81374
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2024-09-22 15:52:43 +0000

    11

--- Basics/String.js
+++ Basics/String.js
@@ -17,4 +17,10 @@ const nw="   ggg   ";
 console.log(nw);
 console.log(nw.trim()); // starting and ending space ko kaat deta h trim()-- trimStart(),trimEnd();
 
+// replace
 
+const url="https://saksham.com//saksham%20thakur"     // browser spaces ko %20 mein change kr deta h 
+console.log(url.replace('%20','replaceitdarling')); //   x=jisse replace krna h , y=jis se replace krwana h
+console.log(url.includes('hihihi')); // tells wheathe rgiven thing is in string ot not
+console.log(game.split('i')); // ye spliit kr dega puri string ko usse variable k basis pr wo ussi vribale
+                             //ko hata deta  string se 
