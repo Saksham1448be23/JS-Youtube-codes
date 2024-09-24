f2f3988c83e0c220f59c62fe7546ee05420c37db
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2024-09-24 11:07:46 +0000

    18

---
+++ Basics02/ObjectDe-Structure.js
@@ -0,0 +1,8 @@
+const course={
+    coursename:"science",
+    price:"999",
+    courseInst:"Me"
+
+}
+const {courseInst:instr }=course; // couser mein se courseInst value ko extract kiya h aur phir usse new name ese hi de kr console mein calll kiya h
+console.log(instr);
