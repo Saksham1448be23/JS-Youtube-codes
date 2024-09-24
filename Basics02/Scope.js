9bd00ad9e7d18e8b58c20641abaf3bbfb5c1b5c2
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2024-09-24 17:52:55 +0000

    22

---
+++ Basics02/Scope.js
@@ -0,0 +1,17 @@
+console.log(addone(4));
+function addone(num){
+    return num+1;
+}
+
+/*
+console.log(addtwo(4));     // ye execute nhi hua kyunki initialisationn baad mein hui aur calling pehle wo kisi or mein daal kr
+const h=function addtwo(num){
+    return num+2;
+}
+*/
+
+console.log(h);   // ye bhi nhi chalega  
+const h=function addtwo(num){
+    return num+2;
+}
+
