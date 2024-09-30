f1641c2c30bd763a4f406986cec94dfecc44f3dd
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2024-09-30 18:12:41 +0000

    30

---
+++ Basics02/Filtermap.js
@@ -0,0 +1,12 @@
+ const sub=[ 1,2,3,4,5,6,7];
+ const newsub=sub.filter( (num)=>{
+    return num>4;
+ })
+ console.log(newsub);
+ 
+ /*             MAP              */
+
+ const sub2=[ 1,2,3,4,5,6,7];
+ const newsub2=sub2.map( (n)=>{ return n+10;} )
+ console.log(newsub2);
+ 
